a4c17f87a55d0213f1b2b048d16df41d14b5af58
System Administrator <root@Soos-MacBook-Pro-2.local>
2014-10-27 01:03:31 +0000

    new column added2

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 <h1>Register here!</h1>
-<p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+<p>Fill in your company name, name and email address, then click <strong>Submit</strong> to register.</p>
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Compnay Name <input type="text" name="company_name" id="company_name"/></br>
       Name  <input type="text" name="name" id="name"/></br>
@@ -49,7 +49,7 @@
         $date = date("Y-m-d");
         // Insert data
         $sql_insert = "INSERT INTO registration_tbl (company_name, name, email, date) 
-                   VALUES (?,?,?)";
+                   VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(4, $company_name);
         $stmt->bindValue(1, $name);
--- index.php~
+++ index.php~
@@ -51,10 +51,10 @@
         $sql_insert = "INSERT INTO registration_tbl (company_name, name, email, date) 
                    VALUES (?,?,?)";
         $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $company_name);
-        $stmt->bindValue(2, $name);
-        $stmt->bindValue(3, $email);
-        $stmt->bindValue(4, $date);
+        $stmt->bindValue(4, $company_name);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $date);
         $stmt->execute();
     }
     catch(Exception $e) {
